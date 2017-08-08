e439f50f0eeaa672b8ace71af36d443da87d02d3
LS-C <suzy.cho111@gmail.com>
2017-08-08 19:03:07 -0400

    solution

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,27 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
+require 'pry'
 
+
+ikea = {:chair => 25, :table => 5, :mattress => 450}
+
+
+def key_for_min_value(name_hash)
+    value = 0
+    hash_key = nil
+    name_hash.each do | item, price|
+        if value == 0
+            value = price
+            hash_key = item
+        elsif value - price > 0
+            value = price
+            hash_key=item
+        end
+    end
+    puts hash_key
 end
+
+
+ key_for_min_value(ikea)
+
